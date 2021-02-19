a8649bcd70e80ba9383412eea105893f251a6b67
salauddin <58369122+salauddin23shumon@users.noreply.github.com>
2021-02-19 12:29:12 +0600

    Update ConstantValues.java

--- app/src/main/java/com/sss/bitm/projectalpha/util/ConstantValues.java
+++ app/src/main/java/com/sss/bitm/projectalpha/util/ConstantValues.java
@@ -7,12 +7,12 @@ public class ConstantValues {
     public static final String MAP_DIRECTION_URL = "https://maps.googleapis.com/maps/api/directions/";
     public static final String PLACE_PHOTO_URL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=";
 
-    public static String api_key = "ac27c3ff05135d5e437f974d0e818186";//weather api key
+    public static String api_key = Enter your api;//weather api key
 
-    public static String Nearby_api_key = "AIzaSyBiPmG9ot3VdJRYghrsG8pPL6VL8ALP4d4";
-    public static String direction_api_key = "AIzaSyBiPmG9ot3VdJRYghrsG8pPL6VL8ALP4d4";
-//    public static String direction_api_key = "AIzaSyCI-4RaDocwneRsw2ryTRPMf7NzGV-F1CE";       deprecated
-    public static String place_api_key = "AIzaSyDdr6xBMPxfhzE7d1RMtwIRltgaWpSGSWk";
+    public static String Nearby_api_key = Enter your api;
+    public static String direction_api_key = Enter your api;
+//    public static String direction_api_key = Enter your api;       deprecated
+    public static String place_api_key = Enter your api;
 
 
     public static final int REQUEST_IMAGE_CAPTURE = 11;
